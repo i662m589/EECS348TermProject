b8bc661b8686f923c332aadb2cbccd2fddf5f861
Ryan Graham <ryangraham05824@outlook.com>
2024-11-25 14:21:51 -0600

    Documentation of stack code.

--- codeImplementation/DataStructures/stack.cpp
+++ codeImplementation/DataStructures/stack.cpp
@@ -3,10 +3,18 @@
 template<typename T>
 class Stack {
 	public:
+		/*
+			A stack data structure.
+			This constructor should be called with a template type supplied, such as with Stack<int>(), Stack<float>(), etc.
+		*/
 		Stack() {
-			// Should be invoked with a type (e.g. Stack<int>(), Stack<float(), etc.)
 			_size = 0;
 		}
+
+		/*
+			Removes the top-most value on the stack and returns it.
+			This function will throw an exception if the stack is empty.
+		*/
 		T Pop() {
 			if (IsEmpty()) {
 				throw "Cannot pop from empty stack!";
@@ -16,23 +24,45 @@ class Stack {
 			_setSize(_size - 1);
 			return toReturn;
 		}
+
+		/*
+			Returns the top-most value on the stack, without altering its contents.
+			This funciton will throw an exception if the stack is empty.
+		*/
 		T Peek() {
 			if (IsEmpty()) {
 				throw "Cannot peek from empty stack!";
 			}
 			return *(_contents + _size - 1);
 		}
+
+		/*
+			Adds an item to the top of the stack.
+			@param {T} item the item to be added to the stack.
+		*/
 		void Add(T item) {
 			_setSize(_size + 1);
 			*(_contents + _size - 1) = item;
 		}
+
+		/*
+			Returns whether the stack is empty.
+		*/
 		bool IsEmpty() {
 			return _size == 0;
 		}
+
+		/*
+			Simply frees the contents of the stack from memory.
+		*/
 		~Stack() {
 			free(_contents);
 		}
 	private:
+		/*
+			Changes the size of the graph.
+			@param {number} size The new size of the graph
+		*/
 		void _setSize(unsigned int size) {
 			if (size == 0) {
 				free(_contents);
