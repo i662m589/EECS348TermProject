b2c31ad23b679142003f8b38f5d613f66da8ebf3
isaacrmiller <isaac.r.miller@icloud.com>
2024-12-05 15:59:44 -0600

    Replaced switch statements with if else statements

--- codeImplementation/evaluator.cpp
+++ codeImplementation/evaluator.cpp
@@ -7,18 +7,28 @@
 using namespace std;
 
 double conv_str_doub(const string &x) {
-    switch(x) {
-        case "0": return 0;
-        case "1": return 1;
-        case "2": return 2;
-        case "3": return 3;
-        case "4": return 4;
-        case "5": return 5;
-        case "6": return 6;
-        case "7": return 7;
-        case "8": return 8;
-        case "9": return 9;
-        default: return 111;
+    if (x == "0") {
+        return 0;
+    } else if ( x == "1") {
+        return 1;
+    } else if ( x == "2") {
+        return 2;
+    } else if ( x == "3") {
+        return 3;
+    } else if ( x == "4") {
+        return 4;
+    } else if ( x == "5") {
+        return 5;
+    } else if ( x == "6") {
+        return 6;
+    } else if ( x == "7") {
+        return 7;
+    } else if ( x == "8") {
+        return 8;
+    } else if ( x == "9") {
+        return 9;
+    } else {
+        return 111;
     }
 // This function will read a character and return that digit if that character is a digit. Otherwise, it will return 111 as an error state.
 };
@@ -45,23 +55,22 @@ double evaluator(vector<string> &vect) {
             double num1 = stack.Pop();
             double num2 = stack.Pop();
 
-            switch (vect[i]) {
-                case "+":
-                    stack.Add(num1 + num2);
-                    break;
-                case "-":
+            if (vect[i] == "+") {
+                stack.Add(num1 + num2);
+                break;
+            } else if (vect[i] == "-") {
+                stack.Add(num1 - num2);
+                break;
+            } else if (vect[i] == "*") {
+                stack.Add(num1 * num2);
+                break;
+            } else if (vect[i] == "/") {
+                if (num2 == 0) {
+                    throw "Cannot divide by zero!";
+                } else {
                     stack.Add(num1 - num2);
-                    break;
-                case "*":
-                    stack.Add(num1 * num2);
-                    break;
-                case "/":
-                    if (num2 == 0) {
-                        throw "Cannot divide by zero!";
-                    } else {
-                        stack.Add(num1 / num2);
-                    }
-                    break;
+                }
+                break;
             }
         }
     }
