91cf63bab7f9797942f2030ab86ced55b89531c0
Ryan Graham <ryangraham05824@outlook.com>
2024-12-10 15:02:34 -0600

    Reimplement stack based on vector, instead of malloc
    I have 99 headaches, and most of them are due to bad memory nonsense.

--- codeImplementation/stack.cpp
+++ codeImplementation/stack.cpp
@@ -1,17 +1,9 @@
-#include <cstdlib>
-#pragma once
+#include <vector>
+using namespace std;
 
 template<typename T>
 class Stack {
 	public:
-		/*
-			A stack data structure.
-			This constructor should be called with a template type supplied, such as with Stack<int>(), Stack<float>(), etc.
-		*/
-		Stack() {
-			_size = 0;
-		}
-
 		/*
 			Removes the top-most value on the stack and returns it.
 			This function will throw an exception if the stack is empty.
@@ -22,7 +14,7 @@ class Stack {
 			}
 
 			T toReturn = Peek();
-			_setSize(_size - 1);
+			_contents.pop_back();
 			return toReturn;
 		}
 
@@ -34,7 +26,7 @@ class Stack {
 			if (IsEmpty()) {
 				throw "Cannot peek from empty stack!";
 			}
-			return *(_contents + _size - 1);
+			return _contents.at(Size() - 1);
 		}
 
 		/*
@@ -42,59 +34,21 @@ class Stack {
 			@param {T} item the item to be added to the stack.
 		*/
 		void Add(T item) {
-			_setSize(_size + 1);
-			*(_contents + _size - 1) = item;
+			_contents.push_back(item);
 		}
 
 		/*
 			Returns whether the stack is empty.
 		*/
 		bool IsEmpty() const {
-			return _size == 0;
+			return Size() == 0;
 		}
 		/*
 			Returns the size of the stack.
 		*/
 		unsigned int Size() const {
-			return _size;
-		}
-
-		/*
-			Simply frees the contents of the stack from memory.
-		*/
-		~Stack() {
-			free(_contents);
+			return _contents.size();
 		}
 	private:
-		/*
-			Changes the size of the graph.
-			@param {number} size The new size of the graph
-		*/
-		void _setSize(unsigned int size) {
-			if (size == 0) {
-				free(_contents);
-				_size = 0;
-				_contents = nullptr;
-				return;
-			}
-
-			unsigned int oldSize = _size;
-			T* newArray = (T*)malloc(sizeof(T) * size);
-
-			// Copy values from old array to new array
-			for (unsigned int i = 0; i < size; i++) {
-				if (i >= oldSize) {
-					break;
-				}
-				*(newArray + i) = *(_contents + i);
-			}
-
-			// Free old array, place new array in it's place
-			free(_contents);
-			_size = size;
-			_contents = newArray;
-		}
-
-		T* _contents;
-		unsigned int _size;
+		vector<T> _contents;
 };
