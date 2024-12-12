040bd7e0c6f6a8170298e073c396400c3250797e
someDecentUsernameHopefully <ryangraham05824@outlook.com>
2024-12-11 22:08:06 -0600

    Make nested unarys work
    I.e. ---1 yields -1

--- codeImplementation/formatter.cpp
+++ codeImplementation/formatter.cpp
@@ -15,15 +15,40 @@ string Format(const string& input) {
 
 	// For each plus or minus sign, check that there is a number preceding it; if not, add a zero.
 	bool lookingForNum = false;
+	int opIndex;
 	for (int i = copy.size() - 1; i >= 0; i--) {
 		char c = copy[i];
 		if (c >= '0' && c <= '9') {
 			lookingForNum = false;
+		} else if (lookingForNum && (c == '+' || c == '-' || c == '*' || c == '/' || c == '%')) {
+			int numIndex = opIndex;
+			int parens = 0;
+			bool foundANum = false;
+			for (; numIndex < (int)copy.size(); numIndex++) {
+				if (copy[numIndex] == '(') {
+					parens++;
+				} else if (copy[numIndex] == ')') {
+					parens--;
+				}
+				if ((copy[numIndex] >= '0' && copy[numIndex] <= '9') || copy[numIndex] == '.') {
+					foundANum = true;
+				}
+				else if (foundANum && parens == 0) {
+					break;
+				}
+			}
+			
+			copy.insert(numIndex, ")");
+			copy.insert(i + 2, "(0");
+			lookingForNum = false;
 		} else if (c == '(' && lookingForNum) {
 			copy.insert(i + 1, "0");
 			lookingForNum = false;
-		} else if (c == '+' || c == '-') {
+		}
+		
+		if (c == '+' || c == '-') {
 			lookingForNum = true;
+			opIndex = i;
 		}
 	}
 	if (lookingForNum) {
